8fed21adef2029caecc60d20dd819bcf8e32b14d
dyehorov <114303640+dyehorov@users.noreply.github.com>
2026-06-29 09:21:21 +0200

    feat: Add Post model and update User model with additional fields

--- server/models/User.js
+++ server/models/User.js
@@ -3,21 +3,48 @@ import mongoose from "mongoose"
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    require: true,
+    required: true,
     unique: true,
   },
   fullName: {
     type: String,
-    require: true,
+    required: true,
   },
   username: {
     type: String,
-    require: true,
+    required: true,
     unique: true,
   },
   password: {
     type: String,
+    required: true,
   },
+  avatar: {
+    type: String,
+    default: "",
+  },
+  bio: {
+    type: String,
+    default: "",
+    maxlength: 150,
+  },
+  website: {
+    type: String,
+    default: "",
+  },
+  followers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+
+  following: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
 })
 
 const User = mongoose.model("User", userSchema)
