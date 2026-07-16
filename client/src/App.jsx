8fe343013ff2f080f688401dac015b784945cb3b
dyehorov <114303640+dyehorov@users.noreply.github.com>
2026-07-16 16:41:54 +0200

    chores: remove unused scripts and components, add route guards

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,38 @@
 import "./App.css"
-import ButtonCTA from "./components/buttonCTA"
-import AuthCard from "./components/authCard"
 import { Routes, Route } from "react-router"
 import Registration from "./pages/registration"
 import Login from "./pages/login"
-import Aside from "./components/aside"
 import MainLayout from "./layouts/mainLayout"
 import Home from "./pages/home"
 import Explore from "./pages/explore"
-// import Messages from "./pages/messages"
+import EditProfile from "./pages/editProfile"
+import Messages from "./pages/messages"
+import Profile from "./pages/profile"
+import ForgotPassword from "./pages/forgotPassword"
+import { BrowserRouter } from "react-router"
+import { ProtectedRoute, PublicRoute } from "./components/routeGuard"
 
 export default function App() {
   return (
-    <>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Registration />} />
+        <Route element={<PublicRoute />}>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Registration />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Route>
 
-        <Route element={<MainLayout />}>
-          <Route path="/home" element={<Home />} />
-          <Route path="/explore" element={<Explore />} />
+        <Route element={<ProtectedRoute />}>
+          <Route element={<MainLayout />}>
+            <Route path="/home" element={<Home />} />
+            <Route path="/explore" element={<Explore />} />
+            <Route path="/messages" element={<Messages />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/profile/:userId" element={<Profile />} />
+            <Route path="/edit-profile" element={<EditProfile />} />
+          </Route>
         </Route>
       </Routes>
-    </>
+    </BrowserRouter>
   )
 }
